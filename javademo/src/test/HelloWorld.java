6f99de424eb7dd7a97cb0299e7dae7e4e5b24b4d
admin <1737695589@qq.com>
2020-11-18 13:06:09 +0800

    这是我用idea第一次提交

--- javademo/src/test/HelloWorld.java
+++ javademo/src/test/HelloWorld.java
@@ -1,9 +1,16 @@
 package test;
 
+import org.junit.Test;
+
 public class HelloWorld {
 
 	public static void main(String[] args){
 		//打印一个helloworld!!
 		System.out.println("Hello world!!");
+	}
+
+	@Test
+	public  void  fun1(){
+		System.out.println("79765465");
 	}
 }
